a63e7abd04178c3686d5c726cec3ad517c2ee74d
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-10-31 20:12:00 +0300

    This commit adds the getters and their tests

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -24,4 +24,76 @@ public class Hero {
         this.squad_id = squad_id;
         instances.add(this);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hero hero = (Hero) o;
+        return id == hero.id &&
+                age == hero.age &&
+//      squad_id == hero.squad_id &&
+                Objects.equals(name, hero.name) &&
+                Objects.equals(origin_story, hero.origin_story) &&
+                Objects.equals(powers, hero.powers) &&
+                Objects.equals(weaknesses, hero.weaknesses);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, origin_story, powers, weaknesses);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setOrigin_story(String origin_story) {
+        this.origin_story = origin_story;
+    }
+
+    public void setPowers(String powers) {
+        this.powers = powers;
+    }
+
+    public void setWeaknesses(String weaknesses) {
+        this.weaknesses = weaknesses;
+    }
+
+    public void setId(int id) { this.id = id ; }
+
+    public void setSquad_id(int squad_id) { this.squad_id = squad_id; }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getOrigin_story() {
+        return origin_story;
+    }
+
+    public String getPowers() {
+        return powers;
+    }
+
+    public String getWeaknesses() {
+        return weaknesses;
+    }
+
+    public int getSquad_id() {
+        return squad_id;
+    }
+
+
 }
