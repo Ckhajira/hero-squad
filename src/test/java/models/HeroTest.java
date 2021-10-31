bd8600f6e61980da49ff9ad753a6c95feaf1b895
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-10-31 15:19:43 +0300

    This commit adds an instatiation to the HeroTest file

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -2,7 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
-
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HeroTest {
@@ -14,4 +14,8 @@ public class HeroTest {
     @After
     public void tearDown() throws Exception {
     }
+    @Test
+    public Hero setupNewHero() {
+    return new Hero("Turi", 22, "He was struck with radi and became the leader  of his mbogi", "spinning with yennx", "broke boi", 6);
+  }
 }
