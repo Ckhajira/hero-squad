dd3799d814d425ecf123819f9c8a55cbe0c4a08e
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-10-31 16:55:23 +0300

    This commit adds the Hero constructor

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -3,7 +3,7 @@ package models;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class Hero{
+public class Hero {
     private int id;
     private String name;
     private int age;
@@ -13,4 +13,15 @@ public class Hero{
     private int squad_id;
     private static ArrayList<Hero> instances = new ArrayList<>();
 
+
+    public Hero(String name, int age, String origin_story, String powers, String weaknesses, int squad_id) {
+//    this.id = instances.size();
+        this.name = name;
+        this.age = age;
+        this.origin_story = origin_story;
+        this.powers = powers;
+        this.weaknesses = weaknesses;
+        this.squad_id = squad_id;
+        instances.add(this);
+    }
 }
