8efefa33579e0fb460be8adf229d578641e2522e
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-10-31 16:47:03 +0300

    This commit adds the variables for the fields

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -4,5 +4,13 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Hero{
+    private int id;
+    private String name;
+    private int age;
+    private String origin_story;
+    private String powers;
+    private String weaknesses;
+    private int squad_id;
+    private static ArrayList<Hero> instances = new ArrayList<>();
 
 }
