46a5882499f8b23e8b7da7b6872847c8f10cdda2
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-10-31 19:56:21 +0300

    This commit adds the add logic to the sqldao file

--- src/main/java/dao/Sql2oHeroDao.java
+++ src/main/java/dao/Sql2oHeroDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Sql2oHeroDao implements HeroDao{
     private final Sql2o sql2o;
-    public Sql2oHeroDao(Sql2o sql2o) {this.sql2o = sql20;}
+    public Sql2oHeroDao(Sql2o sql2o) {this.sql2o = sql2o;}
 
     @Override
     public List<Hero> getAll(){
@@ -15,5 +15,20 @@ public class Sql2oHeroDao implements HeroDao{
                     .executeAndFetch(Hero.class);
         }
     }
-    
+
+    @Override
+    public void add(Hero hero){
+        String sql = "INSERT INTO heroes (name, age, origin_story, powers, weakness, squad_id) VALUES (:name, :age:origin_story, :powers, :weaknesses, :squad_id)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(hero)
+                    .executeUpdate()
+                    .getKey();
+            hero.setId(id);
+
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
 }
