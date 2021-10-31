4bff93b5f8b87eb09593fd49495c59574e8cb91b
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-10-31 19:46:46 +0300

    This commit adds the getall logic to the sqldao file

--- src/main/java/dao/Sql2oHeroDao.java
+++ src/main/java/dao/Sql2oHeroDao.java
@@ -4,3 +4,16 @@ import models.Hero;
 import org.sql2o.*;
 import java.util.List;
 
+public class Sql2oHeroDao implements HeroDao{
+    private final Sql2o sql2o;
+    public Sql2oHeroDao(Sql2o sql2o) {this.sql2o = sql20;}
+
+    @Override
+    public List<Hero> getAll(){
+        try (Connection con=sql2o.open()){
+            return con.createQuery("SELECT * FROM heroes")
+                    .executeAndFetch(Hero.class);
+        }
+    }
+    
+}
